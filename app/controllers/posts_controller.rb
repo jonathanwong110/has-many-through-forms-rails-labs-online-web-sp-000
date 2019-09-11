1a64b60d135713f6bb68c3febcb2bd8d71f25d6f
Jonathan Wong <jonathan.wong110@gmail.com>
2019-09-11 19:28:30 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
     @comment = @post.comments.build
-    user = User.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def index
