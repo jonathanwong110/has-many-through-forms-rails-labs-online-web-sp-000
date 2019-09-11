b7d6eb8a00455825fbe4d6c102bee73f4b7de3bf
Jonathan Wong <jonathan.wong110@gmail.com>
2019-09-11 20:12:14 +0000

    Automatically backed up by Learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,10 +3,4 @@ class Comment < ActiveRecord::Base
   belongs_to :post
   accepts_nested_attributes_for :user, reject_if: proc { |attributes| attributes['username'].blank?}
 
-  def users_attributes=(user_attributes)
-    user_attributes.values.each do |user_attribute|
-      user = User.find_or_create_by(user_attribute)
-      self.users << users
-    end
-  end
 end
